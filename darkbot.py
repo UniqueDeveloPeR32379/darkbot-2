e56434ca91a8a13156d09bd6c6386b150c9332c6
BestCoderz <utk.bkkg@gmail.com>
2018-07-26 19:27:50 +0530

    Update darkbot.py

--- darkbot.py
+++ darkbot.py
@@ -15,7 +15,6 @@ async def on_ready():
 
 
 @client.event
-@client.event
 async def on_message(message):
     user_id = message.author.id
     author_xp = get_xp(user_id)
@@ -42,6 +41,45 @@ async def on_message(message):
                 await client.send_message(message.author, "You didn't got xp!")
         except ValueError:
             await client.send_message(message.channel, "Use it like d!gamble 10")
+
+
+def user_add_xp(user_id: int, xp: int):
+    if os.path.isfile("users.json"):
+        try:
+            with open('users.json', 'r') as fp:
+                users = json.load(fp)
+            users[user_id]['xp'] += xp
+            with open('users.json', 'w') as fp:
+                json.dump(users, fp, sort_keys=True, indent=4)
+        except KeyError:
+            with open('users.json', 'r') as fp:
+                users = json.load(fp)
+            users[user_id] = {}
+            users[user_id]['xp'] = xp
+            with open('users.json', 'w') as fp:
+                json.dump(users, fp, sort_keys=True, indent=4)
+    else:
+        users = {user_id: {}}
+        users[user_id]['xp'] = xp
+        with open('users.json', 'w') as fp:
+            json.dump(users, fp, sort_keys=True, indent=4)
+
+
+def get_xp(user_id: int):
+    if os.path.isfile('users.json'):
+        with open('users.json', 'r') as fp:
+            users = json.load(fp)
+        return users[user_id]['xp']
+    else:
+        return 0
+     
+@client.event
+async def on_message(message):
+
+    if message.content.lower().startswith('d!xp'):
+        await client.send_message(message.channel, "You have `{}` XP!".format(get_xp(message.author.id)))
+
+    user_add_xp(message.author.id, 2)
 
 
 def user_add_xp(user_id: int, xp: int):
