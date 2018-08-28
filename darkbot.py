a631fe1d02eac7c6577982c1b9fe1f286c939d39
BestCoderz <utk.bkkg@gmail.com>
2018-08-28 19:40:01 +0530

    Update darkbot.py

--- darkbot.py
+++ darkbot.py
@@ -1,57 +1,54 @@
 import discord
-import json
-import os.path
 import asyncio
-import random
+import json
+import os
+from discord.ext import commands
 
 client = discord.Client()
-gamble_msg_stuff = {}
- 
 
 @client.event
 async def on_ready():
-    print(client.user.name)
-    print("===================")
-
+    print('bot online')
+  
+@client.event
+async def on_member_join(member):
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+    
+    await update_data(users, member)
+    
+    with open ('users.json', 'w') as f:
+        json.dump(users, f)
+  
 @client.event
 async def on_message(message):
-
-    if message.content.lower().startswith('d!xp'):
-        await client.send_message(message.channel, "You have `{}` XP!".format(get_xp(message.author.id)))
-
-    user_add_xp(message.author.id, 2)
-
-
-def user_add_xp(user_id: int, xp: int):
-    if os.path.isfile("users.json"):
-        try:
-            with open('users.json', 'r') as fp:
-                users = json.load(fp)
-            users[user_id]['xp'] += xp
-            with open('users.json', 'w') as fp:
-                json.dump(users, fp, sort_keys=True, indent=4)
-        except KeyError:
-            with open('users.json', 'r') as fp:
-                users = json.load(fp)
-            users[user_id] = {}
-            users[user_id]['xp'] = xp
-            with open('users.json', 'w') as fp:
-                json.dump(users, fp, sort_keys=True, indent=4)
-    else:
-        users = {user_id: {}}
-        users[user_id]['xp'] = xp
-        with open('users.json', 'w') as fp:
-            json.dump(users, fp, sort_keys=True, indent=4)
-
-
-def get_xp(user_id: int):
-    if os.path.isfile('users.json'):
-        with open('users.json', 'r') as fp:
-            users = json.load(fp)
-        return users[user_id]['xp']
-    else:
-        return 0
-
-     
-
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+        
+    await update_data(users, message.author)
+    await add_experience(users, message.author, 5)
+    await level_up(users, message.author, message.channel)
+                        
+    
+    with open ('users.json', 'w') as f:
+        json.dump(users, f)
+        
+async def update_data(users, user):
+    if not user.id in users:
+        users[user.id] = {}
+        users[user.id]['experience'] = 0
+        users[user.id]['level'] = 1
+    
+async def add_experience(users, user, exp):
+    users[user.id]['experience'] += exp
+    
+async def level_up(users, user, channel):
+    experience = users[user.id]['experience']
+    lvl_start = users[user.id]['level']
+    lvl_end = int(experience ** (1/4))
+    
+    if lvl_start < lvl_end:
+        await client.send_message(channel, '{} has leveled up to level {}'.format(user.mention, lvl_end))
+        users[user.id]['level'] = lvl_end  
+   
 client.run(os.getenv('Token'))
