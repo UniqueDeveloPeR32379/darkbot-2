de675a3f09bca6597303e44f1cd960de909ed80a
BestCoderz <utk.bkkg@gmail.com>
2018-08-28 20:13:35 +0530

    Update darkbot.py

--- darkbot.py
+++ darkbot.py
@@ -11,44 +11,100 @@ async def on_ready():
     print('bot online')
   
 @client.event
-async def on_member_join(member):
-    with open('users.json', 'r') as f:
-        users = json.load(f)
-    
-    await update_data(users, member)
-    
-    with open ('users.json', 'w') as f:
-        json.dump(users, f)
-  
-@client.event
 async def on_message(message):
-    with open('users.json', 'r') as f:
-        users = json.load(f)
+    user_id = message.author.id
+
+    author_level = get_level(user_id)
+    author_xp = get_xp(user_id)
+
+    if author_level == 0 and author_xp >= 100:
+        set_level(user_id, 1)
+        await client.send_message(message.channel, "Congratulations! You have reached Level 1")
+
+    if author_level == 1 and author_xp >= 200:
+        set_level(user_id, 2)
+        await client.send_message(message.channel, "Congratulations! You have reached Level 2")
         
-    await update_data(users, message.author)
-    await add_experience(users, message.author, 5)
-    await level_up(users, message.author, message.channel)
-                        
-    
-    with open ('users.json', 'w') as f:
-        json.dump(users, f)
-        
-async def update_data(users, user):
-    if not user.id in users:
-        users[user.id] = {}
-        users[user.id]['experience'] = 0
-        users[user.id]['level'] = 1
-    
-async def add_experience(users, user, exp):
-    users[user.id]['experience'] += exp
-    
-async def level_up(users, user, channel):
-    experience = users[user.id]['experience']
-    lvl_start = users[user.id]['level']
-    lvl_end = int(experience ** (1/4))
-    
-    if lvl_start < lvl_end:
-        await client.send_message(channel, '{} has leveled up to level {}'.format(user.mention, lvl_end))
-        users[user.id]['level'] = lvl_end  
+    if author_level == 2 and author_xp >= 250:
+        set_level(user_id, 3)
+        await client.send_message(message.channel, "Congratulations! You have reached Level 3")
+    if author_level == 3 and author_xp >= 300:
+        set_level(user_id, 4)
+        await client.send_message(message.channel, "Congratulations! You have reached Level 4")
+    if author_level == 4 and author_xp >= 400:
+        set_level(user_id, 5)
+        await client.send_message(message.channel, "Congratulations! You have reached Level 5")
+    if author_level == 8 and author_xp >= 700:
+        set_level(user_id, 9)
+        await client.send_message(message.channel, "Congratulations! You have reached Level 9")
+    if author_level == 9 and author_xp >= 1000:
+        set_level(user_id, 10)
+        await client.send_message(message.channel, "Congratulations! You have reached 10")
+        lvl_role = None
+        for role in message.server.roles:
+            if role.name == "Regulars":
+                lvl_role = role
+
+        await client.add_roles(message.author, lvl_role)
+
+    if message.content.lower().startswith('d!xp'):
+        await client.send_message(message.channel, "You have `{}` XP!".format(get_xp(message.author.id)))
+
+    if message.content.lower().startswith('d!lvl'):
+        level = get_level(user_id)
+        await client.send_message(message.channel, "You are at Level: {}".format(level))
+
+    user_add_xp(message.author.id, 2)
+
+
+def user_add_xp(user_id: int, xp: int):
+    if os.path.isfile("users.json"):
+        try:
+            with open('users.json', 'r') as fp:
+                users = json.load(fp)
+            users[user_id]['xp'] += xp
+            with open('users.json', 'w') as fp:
+                json.dump(users, fp, sort_keys=True, indent=4)
+        except KeyError:
+            with open('users.json', 'r') as fp:
+                users = json.load(fp)
+            users[user_id] = {}
+            users[user_id]['xp'] = xp
+            with open('users.json', 'w') as fp:
+                json.dump(users, fp, sort_keys=True, indent=4)
+    else:
+        users = {user_id: {}}
+        users[user_id]['xp'] = xp
+        with open('users.json', 'w') as fp:
+            json.dump(users, fp, sort_keys=True, indent=4)
+
+
+def get_xp(user_id: int):
+    if os.path.isfile('users.json'):
+        with open('users.json', 'r') as fp:
+            users = json.load(fp)
+        return users[user_id]['xp']
+    else:
+        return 0
+
+
+def set_level(user_id: int, level: int):
+    if os.path.isfile('users.json'):
+        with open('users.json', 'r') as fp:
+            users = json.load(fp)
+        users[user_id]["level"] = level
+        with open('users.json', 'w') as fp:
+            json.dump(users, fp, sort_keys=True, indent=4)
+
+
+def get_level(user_id: int):
+    if os.path.isfile('users.json'):
+        try:
+            with open('users.json', 'r') as fp:
+                users = json.load(fp)
+            return users[user_id]['level']
+        except KeyError:
+            return 0
+
    
 client.run(os.getenv('Token'))
