a3f7ab3ebb6dd33252bddda81125ae3790e53c8b
BestCoderz <utk.bkkg@gmail.com>
2018-11-26 11:50:22 +0530

    Update darkbot.py

--- darkbot.py
+++ darkbot.py
@@ -11,14 +11,14 @@ client.remove_command('help')
 #show when it connects to discord
 @client.event
 async def on_ready():
-  for channel in author.server.channels:
-    if channel.name == '★verify-for-chatting★':
-    role = discord.utils.get(user.server.roles, name="Verified")
-    while True:
-    reaction = await client.wait_for_reaction(emoji="🇻", message=message)
-    await client.add_roles(reaction.message.author, role)
-    print('Logged in as')
-    print(client.user.name)
+    for channel in author.server.channels:
+        if channel.name == '★verify-for-chatting★':
+            role = discord.utils.get(user.server.roles, name="Verified")
+        while True:
+            reaction = await client.wait_for_reaction(emoji="🇻", message=message)
+            await client.add_roles(reaction.message.author, role)
+            print('Logged in as')
+            print(client.user.name)
         
 @client.command(pass_context = True)
 @commands.has_permissions(administrator=True)
